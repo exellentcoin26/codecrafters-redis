b99f905415a8e4a6ffc7e826441e4f0c963e5912
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-01-27 19:24:41 +0100

    Initial test commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "CodeCrafters Redis rust flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-23.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.follows = "rust-overlay/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... } @ inputs:
+  flake-utils.lib.eachDefaultSystem ( system: 
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+        ];
+      };
+    }
+  );
+}
