ca770fbbbf6b71bce45c158e202ee8cd44b3f836
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-01-27 20:50:20 +0100

    Respond to ping command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use log::{debug, error, info, trace, warn};
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // initialize logging
     env_logger::builder()
         .filter_level(log::LevelFilter::Trace)
@@ -12,14 +15,24 @@ fn main() {
     let listener =
         TcpListener::bind("127.0.0.1:6379").expect("failed binding tcp listener to adress");
 
+    let mut buf = [0u8; 1024];
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 info!("accepted new connection");
+                let len = stream.read(&mut buf)?;
+                let command = std::str::from_utf8(&buf[0..len]).expect("command not valid utf-8");
+
+                assert_eq!(&command, &r"*1\r\n$4\r\nping\r\n");
+
+                stream.write_all(r"+PONG\r\n".as_bytes())?;
             }
             Err(e) => {
                 info!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
