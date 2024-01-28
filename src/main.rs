a490e78209e382c7c942c77f56dacdc120dd2fa8
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-01-28 22:37:53 +0100

    Use anyhow context

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use anyhow::{Context, Result};
 use log::{debug, error, info, trace, warn};
 use std::{
     io::{Read, Write},
     net::TcpListener,
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     // initialize logging
     initialize_logging();
 
     trace!("setting up tcp listener");
     let listener =
-        TcpListener::bind("127.0.0.1:6379").expect("failed binding tcp listener to adress");
+        TcpListener::bind("127.0.0.1:6379").context("failed binding tcp listener to adress")?;
 
     let mut buf = vec![0u8; 512];
 
@@ -19,7 +20,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             Ok(mut stream) => {
                 info!("accepted new connection");
                 let len = stream.read(&mut buf)?;
-                let command = std::str::from_utf8(&buf[0..len]).expect("command not valid utf-8");
+                let command =
+                    std::str::from_utf8(&buf[0..len]).context("command not valid utf-8")?;
 
                 assert_eq!(&command, &"*1\r\n$4\r\nping\r\n");
 
