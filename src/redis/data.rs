992e0d945991cd137fddba4e384e85690f335e83
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-02-09 16:06:57 +0100

    Converted resp parsing to use line iterator

--- src/redis/data.rs
+++ src/redis/data.rs
@@ -2,67 +2,79 @@ use anyhow::{bail, Context, Result};
 
 const CRLF: &str = "\r\n";
 
-const EMPTY_INPUT_ERROR_MSG: &str = "input cannot be empty";
+const EMPTY_INPUT_ERROR_MSG: &str = "cannot parse empty resp string";
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum DataType {
     SimpleString(String),
-    Array(Vec<DataType>),
+    BulkString(Box<[u8]>),
+    Array(Box<[DataType]>),
 }
 
 impl DataType {
+    pub fn from_resp(value: &'_ str) -> Result<Self> {
+        dbg!(value);
+        let mut lines = value.split(CRLF);
+        let result = Self::from_resp_iter(&mut lines)?;
+        let mut remainder = lines.peekable();
+        Ok(match remainder.peek() {
+            Some(&"") | None => result,
+            Some(..) => {
+                unimplemented!(
+                    "could not parse resp string as single value (remaining: {:?})",
+                    remainder.fold(String::new(), |remainder, l| remainder + l)
+                )
+            }
+        })
+    }
+
+    fn from_resp_iter<'a>(lines: &mut impl Iterator<Item = &'a str>) -> Result<Self> {
+        let mut first_line = lines.next().context(EMPTY_INPUT_ERROR_MSG)?.chars();
+        let Some(first_char) = first_line.next() else {
+            bail!("resp line must have a first character indicating the datatype");
+        };
+
+        Ok(match first_char {
+            '+' => Self::parse_as_simple_string(first_line.as_str())?,
+            '*' => Self::parse_as_array(Self::parse_as_u32(first_line.as_str())?, lines)?,
+            '$' => Self::parse_as_bulk_string(
+                Self::parse_as_u32(first_line.as_str())?,
+                lines
+                    .next()
+                    .context("missing second line for bulk string data")?,
+            )?,
+            _ => bail!("unimplemented datatype `{}`", first_char),
+        })
+    }
+
+    fn parse_as_u32(value: &'_ str) -> Result<u32> {
+        value
+            .parse::<u32>()
+            .context("failed to parse value as 32 bit unsigned integer")
+    }
+
     fn parse_as_simple_string(value: &'_ str) -> Result<Self> {
-        if value.chars().next().context(EMPTY_INPUT_ERROR_MSG)? != '+' {
-            bail!("expected first character to be `+` for simple string");
-        }
-        let value = value[1..]
-            .strip_suffix(CRLF)
-            .context(format!(r"RESP lines should end in `{:?}`", CRLF))?;
         if value.contains('\n') || value.contains('\r') {
             bail!(r"input cannot contain `\r` or `\n`");
         }
         Ok(DataType::SimpleString(value.to_string()))
     }
 
-    fn parse_as_array(value: &'_ str) -> Result<Self> {
-        let mut lines = value.split_inclusive(CRLF);
-        let first_line = lines.next().context("input cannot be empty")?;
-
-        if first_line.chars().next().context(EMPTY_INPUT_ERROR_MSG)? != '*' {
-            bail!("expected first character to be `*` for array");
-        }
-        let len = first_line[1..]
-            .strip_suffix(CRLF)
-            .expect("suffix not found (unreachable)")
-            .parse::<u32>()
-            .context("array length should be 32 bit decimal integer")?;
-
-        let mut vec = Vec::with_capacity(len as usize);
-        for line in lines {
-            vec.push(Self::try_from(line)?);
-        }
-        if vec.len() != len as usize {
-            bail!("expected `{}` array elements, got `{}`", len, vec.len());
-        }
-
-        Ok(DataType::Array(vec))
+    fn parse_as_array<'a>(length: u32, lines: &mut impl Iterator<Item = &'a str>) -> Result<Self> {
+        Ok(DataType::Array(
+            (0..length)
+                .into_iter()
+                .map(|_| Self::from_resp_iter(&mut *lines))
+                .collect::<Result<_>>()?,
+        ))
     }
-}
 
-impl TryFrom<&'_ str> for DataType {
-    type Error = anyhow::Error;
-
-    fn try_from(value: &'_ str) -> Result<Self> {
-        // first ascii charactor determines the data type
-        let Some(data_type_char) = value.chars().next() else {
-            bail!("input cannot be empty");
-        };
-
-        match data_type_char {
-            '+' => Self::parse_as_simple_string(value),
-            '*' => Self::parse_as_array(value),
-            _ => bail!("unsupported data type: `{}`", data_type_char),
+    fn parse_as_bulk_string(length: u32, value: &'_ str) -> Result<Self> {
+        if value.len() != length as usize {
+            bail!("bulk string value size is not equal to the expected size (expected: `{}`, got: `{}`)", value.len(), length);
         }
+
+        Ok(DataType::BulkString(Box::from(value.as_bytes())))
     }
 }
 
@@ -72,19 +84,43 @@ mod tests {
 
     #[test]
     fn parse_simple_string() {
-        let simple_string = DataType::try_from("+PONG\r\n").unwrap();
+        let simple_string = DataType::from_resp("+PONG\r\n").unwrap();
         assert_eq!(simple_string, DataType::SimpleString("PONG".to_string()));
     }
 
     #[test]
     fn parse_array() {
-        let array = DataType::try_from("*2\r\n+hello\r\n+world\r\n").unwrap();
+        let array = DataType::from_resp("*2\r\n+hello\r\n+world\r\n").unwrap();
         assert_eq!(
             array,
-            DataType::Array(Vec::from([
+            DataType::Array(Box::from([
                 DataType::SimpleString("hello".to_string()),
                 DataType::SimpleString("world".to_string())
             ]))
-        )
+        );
+    }
+
+    #[test]
+    fn parse_bulk_string() {
+        let bulk_string = DataType::from_resp("$4\r\nECHO\r\n").unwrap();
+        assert_eq!(
+            bulk_string,
+            DataType::BulkString(Box::from("ECHO".as_bytes()))
+        );
+    }
+
+    #[test]
+    fn parse_array_of_bulk_string_and_array_of_simple_string() {
+        let array = DataType::from_resp("*2\r\n$4\r\nquux\r\n*2\r\n+FOO\r\n$3\r\nbar\r\n").unwrap();
+        assert_eq!(
+            array,
+            DataType::Array(Box::from([
+                DataType::BulkString(Box::from("quux".as_bytes())),
+                DataType::Array(Box::from([
+                    DataType::SimpleString("FOO".to_string()),
+                    DataType::BulkString(Box::from("bar".as_bytes()))
+                ]))
+            ]))
+        );
     }
 }
