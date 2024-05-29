9e90b8e29e981fe8c031a192bba37787cb4819ee
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-05-29 15:45:19 +0200

    Format nix flake

--- flake.nix
+++ flake.nix
@@ -10,20 +10,25 @@
     flake-utils.follows = "rust-overlay/flake-utils";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... } @ inputs:
-  flake-utils.lib.eachDefaultSystem ( system: 
-    let
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rust-bin.stable."1.70.0".default
-          bacon
-        ];
-      };
-    }
-  );
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable."1.70.0".default
+            bacon
+          ];
+        };
+      }
+    );
 }
